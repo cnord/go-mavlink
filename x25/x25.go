008bc37cb53d5672768c0338c885b7541f00b8c8
amyasnikov <aleksey.myasnikov@bgoperator.com>
2019-02-14 12:49:16 +0300

    Fix issues after code analize in x25 package

--- x25/x25.go
+++ x25/x25.go
@@ -1,16 +1,18 @@
 package x25
 
-// implements hash.Hash
+// X25 implements hash.Hash
 type X25 struct {
 	crc uint16
 }
 
+// New function create crc calculator
 func New() *X25 {
 	x := &X25{}
 	x.Reset()
 	return x
 }
 
+// WriteByte function append byte into crc accumulation
 func (x *X25) WriteByte(b byte) error {
 	tmp := b ^ byte(x.crc&0xff)
 	tmp ^= (tmp << 4)
@@ -18,6 +20,7 @@ func (x *X25) WriteByte(b byte) error {
 	return nil
 }
 
+// Write function append byte slice into crc accumulation
 func (x *X25) Write(p []byte) (n int, err error) {
 	for _, b := range p {
 		x.WriteByte(b)
@@ -25,15 +28,20 @@ func (x *X25) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+// Sum16 function return accumulated crc
 func (x *X25) Sum16() uint16 { return x.crc }
 
+// Sum function append crc bytes on byte slice
 func (x *X25) Sum(in []byte) []byte {
 	s := x.Sum16()
 	return append(in, byte(s>>8), byte(s))
 }
 
+// Size function return crc size in bytes
 func (x *X25) Size() int { return 2 }
 
+// BlockSize function return crc size in blocks
 func (x *X25) BlockSize() int { return 1 }
 
+// Reset function reset current crc
 func (x *X25) Reset() { x.crc = 0xffff }
