7ecca8a9f39db40872b989179f30454da3f11c73
amyasnikov <aleksei.miasnikov@bg.market>
2020-01-14 22:11:38 +0300

    regenerate examples

--- examples/mavlink2/parser.go
+++ examples/mavlink2/parser.go
@@ -8,6 +8,7 @@ package mavlink
 
 import "sync"
 
+// Parser is a finaly state machine which parse bytes to mavlink.Packet
 type Parser struct {
 	state  MAVLINK_PARSE_STATE
 	packet Packet
@@ -20,86 +21,87 @@ var parsersPool = &sync.Pool{
 	},
 }
 
+// Reset set parser to idle state
 func (p *Parser) Reset() {
 	p.state = MAVLINK_PARSE_STATE_UNINIT
 	p.crc.Reset()
 	p.crc = nil
 }
 
-func (parser *Parser) parseChar(c byte) (*Packet, error) {
-	switch parser.state {
+func (p *Parser) parseChar(c byte) (*Packet, error) {
+	switch p.state {
 	case MAVLINK_PARSE_STATE_UNINIT,
 		MAVLINK_PARSE_STATE_IDLE,
 		MAVLINK_PARSE_STATE_GOT_BAD_CRC,
 		MAVLINK_PARSE_STATE_GOT_GOOD_MESSAGE:
 		if c == magicNumber {
-			parser.crc = NewX25()
-			parser.state = MAVLINK_PARSE_STATE_GOT_STX
+			p.crc = NewX25()
+			p.state = MAVLINK_PARSE_STATE_GOT_STX
 		}
 	case MAVLINK_PARSE_STATE_GOT_STX:
-		parser.packet.Payload = make([]byte, 0, c)
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_LENGTH
+		p.packet.Payload = make([]byte, 0, c)
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_LENGTH
 	case MAVLINK_PARSE_STATE_GOT_LENGTH:
-		parser.packet.InCompatFlags = c
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_INCOMPAT_FLAGS
+		p.packet.InCompatFlags = c
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_INCOMPAT_FLAGS
 	case MAVLINK_PARSE_STATE_GOT_INCOMPAT_FLAGS:
-		parser.packet.CompatFlags = c
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_COMPAT_FLAGS
+		p.packet.CompatFlags = c
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_COMPAT_FLAGS
 	case MAVLINK_PARSE_STATE_GOT_COMPAT_FLAGS:
-		parser.packet.SeqID = c
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_SEQ
+		p.packet.SeqID = c
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_SEQ
 	case MAVLINK_PARSE_STATE_GOT_SEQ:
-		parser.packet.SysID = c
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_SYSID
+		p.packet.SysID = c
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_SYSID
 	case MAVLINK_PARSE_STATE_GOT_SYSID:
-		parser.packet.CompID = c
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_COMPID
+		p.packet.CompID = c
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_COMPID
 	case MAVLINK_PARSE_STATE_GOT_COMPID:
-		parser.packet.MsgID = MessageID(c)
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_MSGID1
+		p.packet.MsgID = MessageID(c)
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_MSGID1
 	case MAVLINK_PARSE_STATE_GOT_MSGID1:
-		parser.packet.MsgID += MessageID(c) << 8
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_MSGID2
+		p.packet.MsgID += MessageID(c) << 8
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_MSGID2
 	case MAVLINK_PARSE_STATE_GOT_MSGID2:
-		parser.packet.MsgID += MessageID(c) << 8 * 2
-		parser.crc.WriteByte(c)
-		parser.state = MAVLINK_PARSE_STATE_GOT_MSGID3
+		p.packet.MsgID += MessageID(c) << 8 * 2
+		p.crc.WriteByte(c)
+		p.state = MAVLINK_PARSE_STATE_GOT_MSGID3
 	case MAVLINK_PARSE_STATE_GOT_MSGID3:
-		parser.packet.Payload = append(parser.packet.Payload, c)
-		parser.crc.WriteByte(c)
-		if len(parser.packet.Payload) == cap(parser.packet.Payload) {
-			parser.state = MAVLINK_PARSE_STATE_GOT_PAYLOAD
+		p.packet.Payload = append(p.packet.Payload, c)
+		p.crc.WriteByte(c)
+		if len(p.packet.Payload) == cap(p.packet.Payload) {
+			p.state = MAVLINK_PARSE_STATE_GOT_PAYLOAD
 		}
 	case MAVLINK_PARSE_STATE_GOT_PAYLOAD:
-		crcExtra, err := dialects.findCrcX(parser.packet.MsgID)
+		crcExtra, err := dialects.findCrcX(p.packet.MsgID)
 		if err != nil {
 			crcExtra = 0
 		}
-		parser.crc.WriteByte(crcExtra)
-		if c != uint8(parser.crc.Sum16()&0xFF) {
-			parser.state = MAVLINK_PARSE_STATE_GOT_BAD_CRC
-			parser.packet = Packet{}
+		p.crc.WriteByte(crcExtra)
+		if c != uint8(p.crc.Sum16()&0xFF) {
+			p.state = MAVLINK_PARSE_STATE_GOT_BAD_CRC
+			p.packet = Packet{}
 			return nil, ErrCrcFail
 		}
-		parser.state = MAVLINK_PARSE_STATE_GOT_CRC1
+		p.state = MAVLINK_PARSE_STATE_GOT_CRC1
 	case MAVLINK_PARSE_STATE_GOT_CRC1:
-		if c == uint8(parser.crc.Sum16()>>8) {
-			parser.packet.Checksum = parser.crc.Sum16()
-			parser.state = MAVLINK_PARSE_STATE_GOT_GOOD_MESSAGE
-			result := parser.packet
-			parser.packet = Packet{}
+		if c == uint8(p.crc.Sum16()>>8) {
+			p.packet.Checksum = p.crc.Sum16()
+			p.state = MAVLINK_PARSE_STATE_GOT_GOOD_MESSAGE
+			result := p.packet
+			p.packet = Packet{}
 			return &result, nil
 		}
-		parser.state = MAVLINK_PARSE_STATE_GOT_BAD_CRC
-		parser.packet = Packet{}
+		p.state = MAVLINK_PARSE_STATE_GOT_BAD_CRC
+		p.packet = Packet{}
 		return nil, ErrCrcFail
 	}
 	return nil, nil
