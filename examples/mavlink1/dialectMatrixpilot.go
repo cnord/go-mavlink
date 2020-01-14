2eb633f267e3ed4a597b11f87bbd7f2f5e7de44b
Aleksey Myasnikov <79263256394@ya.ru>
2020-01-14 21:36:43 +0300

    Update dialectMatrixpilot.go

--- examples/mavlink1/dialectMatrixpilot.go
+++ examples/mavlink1/dialectMatrixpilot.go
@@ -70,7 +70,6 @@ func (m *MatrixpilotFlexifunctionSet) Unpack(p *Packet) error {
 	}
 	m.TargetSystem = uint8(payload[0])
 	m.TargetComponent = uint8(payload[1])
-	return nil
 }
 
 // MatrixpilotFlexifunctionReadReq struct (generated typeinfo)
